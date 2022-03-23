fc0c235f1b6df8a470aec1d24c6fca1b5d8f6e33
Zerplabs-Elanju <elanchezhiyan@zirius.in>
2022-03-23 16:22:37 +0530

    Registeration-completed

--- scripts/login.js
+++ scripts/login.js
@@ -1,10 +1,14 @@
-function showPassword(){
-    var button=document.getElementById("password")
-    if(button.type=="password"){
-        button.type="text"
-        document.getElementById("togglePassword").className="far fa-eye"
+function showPassword() {
+    var button = document.getElementById("password")
+    if (button.type == "password") {
+        button.type = "text"
+        document.getElementById("togglePassword").className = "far fa-eye"
     } else {
-        button.type="password"
-        document.getElementById("togglePassword").className="far fa-eye-slash"
+        button.type = "password"
+        document.getElementById("togglePassword").className = "far fa-eye-slash"
     }
+}
+
+function validate() {
+    return document.forms[0].checkValidity();
 }
