ba92351550ed8270cd87f56a6913373fe657ae7a
Zerplabs-Elanju <elanchezhiyan@zirius.in>
2022-03-30 19:23:46 +0530

    Implementation: -Board

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,12 @@
+function setMax(date) {
+    if (date == 'start-date') {
+        document.getElementById(date).setAttribute('min', new Date().toISOString().split("T")[0])
+        document.getElementById('end-date').value = document.getElementById('start-date').value
+    } else {
+        document.getElementById(date).setAttribute('min', document.getElementById('start-date').value)
+    }
+}
+
 function sidebar() {
     if (document.getElementsByClassName("sidebar")[0].style.display == "flex") {
         document.getElementsByClassName("sidebar")[0].style.display = "none"
@@ -6,60 +15,54 @@ function sidebar() {
     }
 }
 
-function dateValidation(date) {
-    var dateVal = document.getElementById(date).value
-    if (date == 'start-date' && dateVal != null) {
+function dateValidation() {
+    var dateVal = document.getElementById('start-date').value
+    if (dateVal != '') {
         document.getElementById('end-date').disabled = false
-    } else if (new Date(dateVal).getTime() < new Date(document.getElementById('start-date').value).getTime()) {
-        console.log("Hiii");
-        document.getElementById('end-date').focus();
-        document.getElementById('createTask').disabled = true
     } else {
-        document.getElementById('createTask').disabled = false
+        document.getElementById('end-date').value = ''
+        document.getElementById('end-date').disabled = true
     }
 }
 
 function validate(feildId) {
-    // if (document.forms[0].checkValidity()) {
-    //     document.getElementById('createTask').disabled = false
-    // } else {
-    //     document.getElementById('createTask').disabled = true
-    // }
     var formElements = document.forms[0].elements
+    var tag = document.createElement("p");
+    var text;
+    tag.className = 'error-feild'
     for (let index = 0; index < formElements.length; index++) {
-
-        var tag = document.createElement("p");
-        tag.className = 'error-feild'
-        if (formElements[index].validity.valueMissing) {
-            if (formElements[index].parentElement.getElementsByTagName('p')[0] == undefined && formElements[index].id == feildId) {
-
-                var text = document.createTextNode("Feild is required can't be empty");
+        if (formElements[index].parentElement.getElementsByTagName('p')[0] == undefined && formElements[index].id == feildId) {
+            if (formElements[index].validity.valueMissing) {
+                text = document.createTextNode("Feild is required can't be empty");
                 tag.appendChild(text);
                 formElements[index].parentElement.appendChild(tag);
-                break;
+            } else if (formElements[index].validity.patternMismatch) {
+                text = document.createTextNode("Invalid Input for email format 'abc@gmail.com'");
+                tag.appendChild(text);
+                formElements[index].parentElement.appendChild(tag);
             }
-        } else {
-            if (formElements[index].id == feildId && formElements[index].parentElement.getElementsByTagName('p')[0] != undefined) {
-                formElements[index].parentElement.getElementsByTagName('p')[0].remove()
-            }
+
+
+        } else if (formElements[index].id == feildId && formElements[index].parentElement.getElementsByTagName('p')[0] != undefined) {
+            formElements[index].parentElement.getElementsByTagName('p')[0].remove()
         }
 
     }
+    document.getElementById('createTask').disabled = !document.forms[0].checkValidity()
 }
 
 function newTask() {
-    if (document.getElementsByClassName("task-form-container")[0].style.display == "flex") {
-        document.getElementsByClassName("task-form-container")[0].style.display = "none"
-        document.getElementsByClassName("new-task-button")[0].value = "New Task"
+    if (document.getElementsByClassName("new-task-form-container")[0].style.display == "flex") {
+        document.getElementsByClassName("new-task-form-container")[0].style.display = "none"
+        document.getElementsByClassName("button-text")[0].innerText = "New Task"
+        document.getElementById("new-task-button").style.display = "flex"
     } else {
-        document.getElementsByClassName("task-form-container")[0].style.display = "flex"
-        document.getElementsByClassName("new-task-button")[0].value = "Cancel  X"
+        document.getElementsByClassName("new-task-form-container")[0].style.display = "flex"
+        document.getElementById("new-task-button").style.display = "none"
     }
 }
 
 function send() {
-    // var data = new FormData(document.forms[0])
-    // console.log(data)
     var val = document.getElementById('task-form').elements;
     console.log(val)
     var obj = {}
