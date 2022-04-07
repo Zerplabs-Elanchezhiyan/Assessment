f65ae83dae07c5451acaf934cdf9d0f49ed8f62b
Zerplabs-Elanju <elanchezhiyan@zirius.in>
2022-04-07 11:29:09 +0530

    clear

--- scripts/login.js
+++
@@ -1,8 +0,0 @@
-function showPassword(){
-    var button=document.getElementById("password")
-    if(button.type=="password"){
-        button.type="text"
-    } else {
-        button.type="password"
-    }
-}
